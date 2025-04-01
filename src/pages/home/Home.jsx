a19d9a1a6be145cbac4defb7f7d0709874987421
LENOVO <LENOVO@DESKTOP-VEBADH8.(none)>
2025-04-01 08:45:03 +0200

    home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,17 @@
+import { Box, Link } from '@mui/material';
 import React from 'react';
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-    return (
-        <div>
-            home
-        </div>
+    const navigate = useNavigate();
+
+    return (<Box>
+         {/* Sign Up Button */}
+         <Link onClick={() => navigate("/register")} sx={{ color: "#007BFF", cursor: "pointer" }}>
+          Sign Up
+        </Link>
+    </Box>
+      
     );
 }
 
