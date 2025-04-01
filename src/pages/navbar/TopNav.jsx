d1c5625b19284bfeb54f3d5d9154e417e014bda4
LENOVO <LENOVO@DESKTOP-VEBADH8.(none)>
2025-04-01 16:11:31 +0200

    hello

--- src/pages/navbar/TopNav.jsx
+++ src/pages/navbar/TopNav.jsx
@@ -3,19 +3,12 @@ import { useState } from "react";
 import {
   Box,
   IconButton,
-  InputBase,
-  Typography,
-  Select,
-  MenuItem,
-  FormControl,
   useTheme,
   useMediaQuery,
-  BottomNavigation,
   Button
 } from "@mui/material";
 import GroupsIcon from '@mui/icons-material/Groups';
 import {
-  Search,
   Message,
   DarkMode,
   LightMode,
@@ -23,25 +16,20 @@ import {
   Help,
   Menu,
   Close,
-  Groups
 } from "@mui/icons-material";
-import { useDispatch, useSelector } from "react-redux";
-import { setMode, setLogout } from "../../state";
-import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setMode} from "../../state";
 import FlexBetween from "../../styles/FlexBetween";
 
 const TopNav = () => {
   const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
   const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const user = useSelector((state) => state.user);
   const isNonMobileScreens = useMediaQuery("(min-width: 2000px)");
 
   const theme = useTheme();
   const neutralLight = theme.palette.neutral.light;
   const dark = theme.palette.neutral.dark;
   const background = theme.palette.background.default;
-  const primaryLight = theme.palette.primary.light;
   const alt = theme.palette.background.alt;
 
 
